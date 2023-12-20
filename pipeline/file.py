1ef6847c62a03042505797b1b64e282177eb6e2a
ahjayzz <anton95070@gmail.com>
2023-12-20 03:48:43 -0800

    feat: add class

--- pipeline/file.py
+++ pipeline/file.py
@@ -1,6 +1,6 @@
 import requests
 from utils.config import config
-
+from utils.tools import format_actor_string
 
 DEFAULT_HEADER = "application/json"
 MODULE_NAME = 'file_operation'
@@ -17,24 +17,46 @@ TYPE_DICT = {
     'model': "Model",
 }
 
+print(MODULE_NAME)
+
 
 class FileUtility:
-    def upload(file=None):
+
+    def __init__(self, actor_type, file, uid_value) -> None:
+        self.actor_type = actor_type
+        self.file = file
+        self.uid_value = uid_value
+
+    def upload(self):
         """
             Upload file to file server
         """
-        pass
+        try:
+            formatted_actor_type = format_actor_string(self.actor_type)
+            response = requests.post(
+                url=f"{config['FILE_SERVER_PROTOCAL']}//{config['FILE_SERVER_HOST']}:{config['FILE_SERVER_PORT']}/{config['FILE_SERVER_API_PREFIX']}/{config['FILE_SERVER_API_VERSION']}/{MODULE_NAME}/{formatted_actor_type}FileManager/upload",
+                headers=DEFAULT_HEADER,
+                files=self.file
+            )
+            print(response, response.json())
+        except Exception as e:
+            print(str(e))
+            raise ConnectionError(e)
 
-    def download(type=None):
+    def download(self):
         """
             Download file from file server
         """
         try:
+            formatted_actor_type = format_actor_string(self.actor_type)
             response = requests.post(
-                url=f"{config['FILE_SERVER_PROTOCAL']}//{config['FILE_SERVER_HOST']}:{config['FILE_SERVER_PORT']}/{config['FILE_SERVER_API_PREFIX']}/{config['FILE_SERVER_API_VERSION']}/{config['MODULE_NAME']}/{type}FileManager/download",
+                url=f"{config['FILE_SERVER_PROTOCAL']}//{config['FILE_SERVER_HOST']}:{config['FILE_SERVER_PORT']}/{config['FILE_SERVER_API_PREFIX']}/{config['FILE_SERVER_API_VERSION']}/{MODULE_NAME}/{formatted_actor_type}FileManager/download",
                 headers=DEFAULT_HEADER,
-                json={}
+                json={
+                    f"{self.actor_type}_uid": self.uid_value
+                }
             )
+            print(response, response.json())
         except Exception as e:
             print(str(e))
             raise ConnectionError(e)
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,8 @@
-from pipeline.compress import CompressionUtility
 from pipeline.file import FileUtility
-from pipeline.logging import LogsUtility
+
+file_manager = FileUtility()
+
+
+file_manager.actor_type = 'model'
+file_manager.uid_value = '27c01ebc-d960-4de5-801c-3a8786459ec6'
+file_manager.download()
