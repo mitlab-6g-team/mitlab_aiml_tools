b46ccb0467950567eebc28cf80e7ad0c8a72cde3
ahjayzz <anton95070@gmail.com>
2023-12-21 23:26:53 -0800

    feat: add setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ setup(
     name='mitlab_aiml_tools',
     version='0.1.0',
     author="MITLAB",
+    license='MIT License',
+    url='https://github.com/mitlab-6g-team/mitlab_aiml_tools.git',
     author_email="mitlab6g.project@gmail.com",
     packages=find_packages(),
+    description='MITLab AI/ML tools for pipeline development.',
     install_requires=['requests', 'python-dotenv'],
     python_requires='>=3'
 )
