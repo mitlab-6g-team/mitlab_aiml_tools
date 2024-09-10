fcc5d4d1796526eca6f8978e01344b582fb95066
jasper <jasper33977296@gmail.com>
2024-09-10 10:55:10 -0700

    fix:test SDK print file_path

--- mitlab_aiml_tools/pipeline/file.py
+++ mitlab_aiml_tools/pipeline/file.py
@@ -140,7 +140,6 @@ class FileUtility:
             # Construct the upload URL
             url = f"{self.protocal}://{self.host}:{self.port}/{self.api_prefix}/{self.api_version}/{MODULE_NAME}/GeneralFileManager/upload"
             
-            print("file:"+file)
             print("file_path:"+file_path)
 
             # Perform the file upload
